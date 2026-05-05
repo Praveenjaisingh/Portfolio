2ce1dc1b10fe2b8ce61fd113d22c7bc222cc0755
Praveen <dpp469926@gmail.com>
2026-05-05 10:29:09 +0530

    code updated

--- portfolio/api/index.js
+++ portfolio/api/index.js
@@ -1,13 +1,15 @@
-
-const express = require("express");
 const serverless = require("serverless-http");
+const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
+const portfolioRoutes = require("../src/routes/portfolio.routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
-const portfolioRoutes = require("../src/routes/portfolio.routes");
 app.use("/", portfolioRoutes);
 
 module.exports.handler = serverless(app);

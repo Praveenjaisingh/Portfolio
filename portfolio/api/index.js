a6479d0552747d71a5a0b78166081b48711b0048
Praveen <dpp469926@gmail.com>
2026-05-05 11:03:46 +0530

    code updated

--- portfolio/api/index.js
+++ portfolio/api/index.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/", portfolioRoutes);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use("/api", portfolioRoutes);
 
 module.exports.handler = serverless(app);

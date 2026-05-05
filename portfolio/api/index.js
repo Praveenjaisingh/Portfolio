a4d045f9d18d8cd7f71592c2c5e0ca955a138228
Praveen <dpp469926@gmail.com>
2026-05-05 11:19:52 +0530

    fix routing

--- portfolio/api/index.js
+++ portfolio/api/index.js
@@ -1,18 +1,12 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const portfolioRoutes = require("../src/routes/portfolio.routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 app.use("/api", portfolioRoutes);
 
